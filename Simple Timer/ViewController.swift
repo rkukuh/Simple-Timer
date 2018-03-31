f49acb09d5cf21ac6b24bfdc25cb52e8d2cf20a7
R. Kukuh <rkukuh@gmail.com>
2018-03-31 21:04:30 +0700

    Define logic to run (play, stop, etc) the timer

--- Simple Timer/ViewController.swift
+++ Simple Timer/ViewController.swift
@@ -9,9 +9,65 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var tick: Int = 60
+    var timer = Timer()
+    
+    @IBOutlet weak var tickLabel: UILabel!
+    @IBOutlet weak var playButton: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
+    @objc func decreaseTick() {
+        
+        if tick > 0 {
+            
+            tick -= 1
+            
+            tickLabel.text = String(tick)
+            
+        } else {
+            
+            timer.invalidate()
+            
+            playButton.isEnabled = true
+        }
+    }
+    
+    @IBAction func playDidPress(_ sender: Any) {
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self,
+                                     selector: #selector(decreaseTick),
+                                     userInfo: nil, repeats: true)
+        
+        playButton.isEnabled = false
+    }
+    
+    @IBAction func pauseDidPress(_ sender: Any) {
+        timer.invalidate()
+        
+        playButton.isEnabled = true
+    }
+    
+    @IBAction func minusDidPress(_ sender: Any) {
+        
+        if tick > 10 {
+            
+            tick -= 10
+        }
+        
+        tickLabel.text = String(tick)
+    }
+    
+    @IBAction func resetDidPress(_ sender: Any) {
+        tick = 60
+        tickLabel.text = String(tick)
+    }
+    
+    @IBAction func plusDidPress(_ sender: Any) {
+        tick += 10
+        tickLabel.text = String(tick)
+    }
 }
